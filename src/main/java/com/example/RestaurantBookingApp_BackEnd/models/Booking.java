1faabe286bc39c50f1d4f04914e67208446bb6b1
maryan axmed <12maxmed@gmail.com>
2023-06-07 17:43:25 +0100

    booking model done

--- src/main/java/com/example/RestaurantBookingApp_BackEnd/models/Booking.java
+++ src/main/java/com/example/RestaurantBookingApp_BackEnd/models/Booking.java
@@ -1,4 +1,92 @@
 package com.example.RestaurantBookingApp_BackEnd.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@jakarta.persistence.Table(name = "bookings")
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+//    many to one => customer
+    @ManyToOne
+    @JsonIgnoreProperties({"listOfBookings"})
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+//    one to many => restaurant
+    @OneToMany
+    @JsonIgnoreProperties({"tables", "bookings"})
+    @JoinColumn(name="restaurant_id")
+    private Restaurant restaurant;
+
+    @Column
+    private List<Table> listOfTables;
+
+    @Column
+    private LocalDateTime dateAndTime;
+
+//    CONSTRUCTOR
+
+    public Booking(Customer customer, Restaurant restaurant, LocalDateTime dateAndTime){
+        this.customer = customer;
+        this.restaurant = restaurant;
+        this.listOfTables = new ArrayList<>();
+        this.dateAndTime = dateAndTime;
+    }
+
+//    DEFAULT CONSTRUCTOR
+    public Booking(){
+
+    }
+
+//    GETTERS AND SETTERS:
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public List<Table> getListOfTables() {
+        return listOfTables;
+    }
+
+    public void setListOfTables(List<Table> listOfTables) {
+        this.listOfTables = listOfTables;
+    }
+
+    public LocalDateTime getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(LocalDateTime dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
 }
